bfd3d8e98a636c60f3f38c7cfc89bf4a8a053b70
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-30 19:56:20 +0600

    fetch user information using serializer

--- password_manager/serializers.py
+++ password_manager/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from users.serializers import CustomUserSerializer
+
 from .models import PasswordStore
 
 
 class PasswordStoreSerializer(serializers.ModelSerializer):
+    owner = CustomUserSerializer()
     class Meta:
         model = PasswordStore
         fields = "__all__"
