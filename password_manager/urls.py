06d90e7edc765ca694ce982b6a4c0fa4f29d30b8
Sharif Khan <sharifkhan74647464@gmail.com>
2024-07-30 20:22:23 +0600

    update password manager url

--- password_manager/urls.py
+++ password_manager/urls.py
@@ -1,11 +1,28 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-from . import views
+from .views import PasswordStoreViewSet
 
-router = DefaultRouter()
-router.register(r'password-store', views.PasswordStoreViewSet, basename='password-store')
+store_get = PasswordStoreViewSet.as_view({
+    "get": "retrieve"
+})
+
+store_post = PasswordStoreViewSet.as_view({
+    "post": "create"
+})
+
+store_update = PasswordStoreViewSet.as_view({
+    "patch": "update"
+})
+
+
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("password-store/", include((
+        [
+            path("<int:pk>", store_get, name="password-store-list"),
+            path("", store_post, name="password-store-create"),
+            path("<int:pk>", store_update, name="password-store-update"),
+        ]
+    )))
+    
 ]
