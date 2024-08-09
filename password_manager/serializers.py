fae932780b26d7a062b9718eef5ee275094194bc
Sharif Khan <sharifkhan74647464@gmail.com>
2024-08-09 11:06:52 +0600

    password create method updated

--- password_manager/serializers.py
+++ password_manager/serializers.py
@@ -6,7 +6,13 @@ from .models import PasswordStore
 
 
 class PasswordStoreSerializer(serializers.ModelSerializer):
-    owner = CustomUserSerializer()
+    owner = CustomUserSerializer(read_only=True)
     class Meta:
         model = PasswordStore
         fields = "__all__"
+    
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        password = PasswordStore.objects.create(owner=request.user, **validated_data)
+        return password
--- password_manager/urls.py
+++ password_manager/urls.py
@@ -2,13 +2,11 @@ from django.urls import include, path
 
 from .views import PasswordStoreViewSet
 
-store_list = PasswordStoreViewSet.as_view({
-    "get": "list"
-})
-
-store_post = PasswordStoreViewSet.as_view({
+store = PasswordStoreViewSet.as_view({
+    "get": "list",
     "post": "create"
 })
+
 
 snippet_detail = PasswordStoreViewSet.as_view({
     "get": "retrieve",
@@ -21,8 +19,7 @@ snippet_detail = PasswordStoreViewSet.as_view({
 urlpatterns = [
     path("password-store/", include((
         [
-            path("", store_list, name="password-store-list"),
-            path("", store_post, name="password-store-create"),
+            path("", store, name="password-store"),
             path("<int:pk>", snippet_detail, name="password-store-details"),
         ]
     )))
